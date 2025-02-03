4739e135223a92c5844e0d40d41c3a8d589455b8
unknown <XX236@ccs.local>
2025-02-03 12:50:55 +0800

    commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel_room_reservation_and_management_system.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
