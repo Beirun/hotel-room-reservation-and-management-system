8cd9cc53689a26c6b666f729f29a80f155757243
unknown <XX236@ccs.local>
2025-02-03 12:25:38 +0800

    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hotel_room_reservation_and_management_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
